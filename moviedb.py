75a56fce0ebd6db6544f3b9ffe235205a37b2038
Courey J <coureyfrance@gmail.com>
2026-09-11 14:59:01 -0400

    added movie DB tasks

---
+++ moviedb.py
@@ -0,0 +1,21 @@
+# # Movie DB
+# - [ ] Add a new movie: 
+
+# - [ ] Edit a movie: 
+
+# - [ ] Delete a movie: 
+
+# - [ ] View all movies: 
+
+# - [ ] Search movies: 
+
+# - [ ] Save and load data: 
+
+# - [ ] Error handling: 
+
+# - [ ] Data validation: 
+res = True
+if res:
+    print("the response is true")
+else:
+    print('the response is false')
